f258eefa8e6eda534467b483d8d2bdd40bba4800
Oleksandr Masniuk <nephestdev@gmail.com>
2024-03-01 16:59:02 +0200

    remove placeholders

--- reveal-sc2-opponent.bat
+++ reveal-sc2-opponent.bat
@@ -1,1 +1,1 @@
-start powershell -NoExit -ExecutionPolicy bypass -File Reveal-Sc2Opponent.ps1 -CharacterId pulseNumericId -Race lowercaserace -FilePath opponent.txt -Limit 3
+start powershell -NoExit -ExecutionPolicy bypass -File Reveal-Sc2Opponent.ps1 -FilePath opponent.txt -Limit 3
